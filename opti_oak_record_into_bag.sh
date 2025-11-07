4b0773ebd46a23f68f7c7620b150ab69fc66fb68
molnarszilard <molnarszilard10@gmail.com>
2025-11-07 09:56:00 +0200

    slight update

--- opti_oak_record_into_bag.sh
+++ opti_oak_record_into_bag.sh
@@ -17,17 +17,17 @@ tmux selectp -t 3    # go back to the first pane
 tmux splitw -h -p 50 # split it into two halves
 
 
-# Run the teleop.py script in the second pane
+# Run the optitrack node
 tmux select-pane -t 0
 tmux send-keys "useros1" Enter
 tmux send-keys "source /home/rocon/catkin_ws_optitrack/devel/setup.bash" Enter
 tmux send-keys "roslaunch mocap_optitrack mocap.launch" Enter
 
-# Run the roslaunch command in the first pane
+# Run the rosaria node
 tmux select-pane -t 1
 tmux send-keys "useros1" Enter
 tmux send-keys "source /home/rocon/catkin_ws/devel/setup.bash" Enter
-tmux send-keys "rosrun rosaria RosAria" Enter
+tmux send-keys "rosrun rosaria RosAria --sonar false" Enter
 
 # Run the ros1_bridge on the last panel
 tmux select-pane -t 2
@@ -36,20 +36,20 @@ tmux send-keys "useros2" Enter
 # tmux send-keys "export ROS_MASTER_URI=http://localhost:11311" Enter
 tmux send-keys "ros2 run ros1_bridge dynamic_bridge --bridge-all-topics" Enter
 
-# Run the teleop.py script in the second pane
 tmux select-pane -t 3
+tmux send-keys "distrobox enter example-nvidia-toolkit" Enter
+tmux send-keys "ros2 launch depthai_ros_driver camera.launch.py config:=/home/rocon/.local/distrobox/home/cuda/oak_record_calib.yaml" Enter # This line will not be run...
+
+# Run the teleop.py script
+tmux select-pane -t 4
 tmux send-keys "useros1" Enter
 tmux send-keys "rosrun teleop_twist_keyboard teleop_twist_keyboard.py /cmd_vel:=/RosAria/cmd_vel" Enter
 
-tmux select-pane -t 4
-tmux send-keys "distrobox enter example-nvidia-toolkit" Enter
-tmux send-keys "ros2 launch depthai_ros_driver camera.launch.py" Enter
-
-# Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
 tmux select-pane -t 5
 tmux send-keys "useros2" Enter
 today=$(date +"%Y_%m_%d"_%H_%M_%S)
-tmux send-keys "ros2 bag record -o oak_opti_cam_imu_c24_${today} --all" # Enter
+# tmux send-keys "ros2 bag record -o oak_opti_cam_imu_c24_${today} --all" # Enter
+tmux send-keys "ros2 bag record -o oak_opti_cam_imu_c24_${today} /Robot_1/pose /RosAria/cmd_vel /oak/imu/data /oak/rgb/camera_info /oak/rgb/image_raw /oak/stereo/camera_info /oak/stereo/image_raw /tf" # Enter
 
 tmux select-pane -t 4
 
