5f0fd965a54a7f74be0b6f14d017771d56ae6968
molnarszilard <molnarszilard10@gmail.com>
2025-11-10 10:41:29 +0200

    add upcam record launcher

---
+++ p3_upcam_compressed_record_bag.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Create a new tmux session
+session_name="record_bag_$(date +%s)"
+tmux new-session -d -s $session_name
+
+# Split the window into three panes
+tmux selectp -t 0    # select the first (0) pane
+tmux splitw -v -p 50 # split it into two halves
+tmux selectp -t 0    # go back to the first pane
+tmux splitw -h -p 50 # split it into two halves
+tmux selectp -t 2    # go back to the first pane
+tmux splitw -h -p 50 # split it into two halves
+
+# Run the roslaunch command in the first pane
+tmux select-pane -t 0
+tmux send-keys "useros1" Enter
+tmux send-keys "source /home/rocon/catkin_ws/devel/setup.bash" Enter
+tmux send-keys "roslaunch visnav_p3.launch" Enter
+
+# Run the teleop.py script in the second pane
+tmux select-pane -t 1
+tmux send-keys "useros1" Enter
+tmux send-keys "rosrun teleop_twist_keyboard teleop_twist_keyboard.py /cmd_vel:=/RosAria/cmd_vel" Enter
+
+# Run the optitrack node
+tmux select-pane -t 2
+tmux send-keys "useros1" Enter
+tmux send-keys "source /home/rocon/catkin_ws_optitrack/devel/setup.bash" Enter
+tmux send-keys "roslaunch mocap_optitrack mocap.launch" Enter
+
+# Change the directory to ../topomaps/bags and run the rosbag record command in the third pane
+tmux select-pane -t 3
+tmux send-keys "useros1" Enter
+tmux send-keys "cd ../topomaps/bags" Enter
+tmux send-keys "rosbag record /usb_cam/image_raw/compressed /usb_cam/image_raw/compressed/parameter_descriptions /usb_cam/image_raw/compressed/parameter_updates /usb_cam/camera_info /RosAria/pose /RosAria/cmd_vel /Robot_1/pose /Robot_1/ground_pose" # change topic if necessary
+
+# Attach to the tmux session
+tmux -2 attach-session -t $session_name
+
+#### To kill it from another terminal run: tmux kill-server     
